178fdc6f194cd40a03ac7af80c23c6caa10c5d13
Saurav_Kalsoor <saurav.kalsoor@gmail.com>
2022-01-04 18:56:39 +0530

    Day 4 Completed

--- day4question1.cpp
+++ day4question1.cpp
@@ -1,35 +1,15 @@
-void heapify(vll &arr, ll n, ll i){
-    ll left = 2*i + 1;
-    ll right = 2*i + 2;
-
-    if(left >= n) return;
-
-    ll mx = left;
-    if(right < n && arr[right] > arr[left]) mx = right;
-
-    if(arr[i] < arr[mx]){
-        swap(arr[i], arr[mx]);
-        heapify(arr, n, mx);
-    }
-}
-
-ll getTop(vll &arr, ll &n){
-    ll mx = arr[0];
-    arr[0] = arr[n-1];
-    n--;
-    heapify(arr, n, 0);
-    return mx;
-}
-
 vector<ll> getTopTen(vll &arr, ll n){
-    vll res(10);
-
-    for(ll i=(n/2)-1; i >= 0; i--)
-        heapify(arr, n, i);
-
-    for(ll i=0;i < 10; i++){
-        res[i] = getTop(arr, n);
+    priority_queue<ll, vector<ll>, greater<ll>> mnHeap;  
+    vll res;
+    for (ll a : arr) {
+        mnHeap.push(a);
+        if (mnHeap.size() > 10) 
+            mnHeap.pop();
     }
 
+    while(!mnHeap.empty()){
+        res.push_back(mnHeap.top());
+        mnHeap.pop();
+    }
     return res;
 }
